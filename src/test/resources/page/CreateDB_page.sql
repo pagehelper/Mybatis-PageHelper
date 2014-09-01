e1ba1d76b7e2274a60075e55b2c8f979abb5bf95
isea533 <abel533@gmail.com>
2014-09-01 21:55:21 +0800

    增加分页合理化的测试。

---
+++ src/test/resources/page/CreateDB_page.sql
@@ -0,0 +1,15 @@
+﻿drop table country if exists;
+
+create table country (
+  id integer,
+  countryname varchar(32),
+  countrycode varchar(2)
+);
+
+insert into country (id, countryname, countrycode) values(1,'Angola','AO');
+insert into country (id, countryname, countrycode) values(2,'Afghanistan','AF');
+insert into country (id, countryname, countrycode) values(3,'Albania','AL');
+insert into country (id, countryname, countrycode) values(4,'Algeria','DZ');
+insert into country (id, countryname, countrycode) values(5,'Andorra','AD');
+insert into country (id, countryname, countrycode) values(6,'Anguilla','AI');
+insert into country (id, countryname, countrycode) values(7,'Antigua and Barbuda','AG');
