2fd6aa416534110bc7b3a936c5019094ad85fbf9
isea533 <abel533@gmail.com>
2014-09-22 22:17:47 +0800

    增加对5种参数类型的测试。

--- src/test/java/com/github/pagehelper/mapper/CountryMapper.java
+++ src/test/java/com/github/pagehelper/mapper/CountryMapper.java
@@ -5,12 +5,13 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.RowBounds;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CountryMapper {
 
     List<Country> selectAll();
 
-    List<Country> selectGreterThanId(@Param("id") int id);
+    List<Country> selectGreterThanId(int id);
 
     List<Country> selectGreterThanIdAndNotEquelContryname(@Param("id") int id, @Param("countryname") String countryname);
 
@@ -19,4 +20,10 @@ public interface CountryMapper {
     List<Country> selectAllOrderby();
 
     List<Country> selectAllOrderByParams(@Param("order1") String order1, @Param("order2") String order2);
+
+
+    //下面是三种参数类型的测试方法
+    List<Country> selectAllOrderByMap(Map orders);
+    List<Country> selectAllOrderByList(List<Integer> params);
+    List<Country> selectAllOrderByArray(Integer[] params);
 }
