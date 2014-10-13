097c5d5f43133b21e70fdae5d6404b1aef4839d7
abel533 <abel533@gmail.com>
2014-10-13 15:54:32 +0800

    更新，替换参数。

--- src/test/java/com/github/pagehelper/sql/SqlHelper.java
+++ src/test/java/com/github/pagehelper/sql/SqlHelper.java
@@ -25,17 +25,25 @@
 package com.github.pagehelper.sql;
 
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.mapping.ParameterMode;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 import org.apache.ibatis.reflection.factory.ObjectFactory;
 import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
 import org.apache.ibatis.reflection.wrapper.ObjectWrapperFactory;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.TypeHandlerRegistry;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -147,7 +155,81 @@ public class SqlHelper {
      * @return
      */
     public static String getNamespaceSql(SqlSession session, String namespace, Object params) {
-        return session.getConfiguration().getMappedStatement(namespace).getBoundSql(params).getSql();
+        Configuration configuration = session.getConfiguration();
+        MappedStatement mappedStatement = configuration.getMappedStatement(namespace);
+        TypeHandlerRegistry typeHandlerRegistry = mappedStatement.getConfiguration().getTypeHandlerRegistry();
+        BoundSql boundSql = mappedStatement.getBoundSql(params);
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+        String sql = boundSql.getSql();
+        if (parameterMappings != null) {
+            for (int i = 0; i < parameterMappings.size(); i++) {
+                ParameterMapping parameterMapping = parameterMappings.get(i);
+                if (parameterMapping.getMode() != ParameterMode.OUT) {
+                    Object value;
+                    String propertyName = parameterMapping.getProperty();
+                    if (boundSql.hasAdditionalParameter(propertyName)) {
+                        value = boundSql.getAdditionalParameter(propertyName);
+                    } else if (params == null) {
+                        value = null;
+                    } else if (typeHandlerRegistry.hasTypeHandler(params.getClass())) {
+                        value = params;
+                    } else {
+                        MetaObject metaObject = configuration.newMetaObject(params);
+                        value = metaObject.getValue(propertyName);
+                    }
+                    JdbcType jdbcType = parameterMapping.getJdbcType();
+                    if (value == null && jdbcType == null) jdbcType = configuration.getJdbcTypeForNull();
+                    sql = replaceParameter(sql, value, jdbcType, parameterMapping.getJavaType());
+                }
+            }
+        }
+
+        return sql;
+    }
+
+    /**
+     * 根据类型替换参数
+     * 仅作为数字和字符串两种类型进行处理，需要特殊处理的可以继续完善这里
+     *
+     * @param sql
+     * @param value
+     * @param jdbcType
+     * @param javaType
+     * @return
+     */
+    private static String replaceParameter(String sql, Object value, JdbcType jdbcType, Class javaType) {
+        String strValue = String.valueOf(value);
+        if (jdbcType != null) {
+            switch (jdbcType) {
+                //数字
+                case BIT:
+                case TINYINT:
+                case SMALLINT:
+                case INTEGER:
+                case BIGINT:
+                case FLOAT:
+                case REAL:
+                case DOUBLE:
+                case NUMERIC:
+                case DECIMAL:
+                    break;
+                //日期
+                case DATE:
+                case TIME:
+                case TIMESTAMP:
+                //其他，包含字符串和其他特殊类型
+                default:
+                    strValue = "'" + strValue + "'";
+
+
+            }
+        } else if (Number.class.isAssignableFrom(javaType)) {
+            //不加单引号
+        }
+        else {
+            strValue = "'" + strValue + "'";
+        }
+        return sql.replaceFirst("\\?", strValue);
     }
 
     /**
