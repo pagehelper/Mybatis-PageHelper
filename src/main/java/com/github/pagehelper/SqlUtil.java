90104284302d80e3b92d470d618cb48a48a3a858
abel533 <abel533@gmail.com>
2014-10-22 11:26:51 +0800

    【两个sql解析库】这次提交包含两种解析库，下次提交会去掉老的方式。

--- src/main/java/com/github/pagehelper/SqlUtil.java
+++ src/main/java/com/github/pagehelper/SqlUtil.java
@@ -27,6 +27,10 @@ package com.github.pagehelper;
 import com.foundationdb.sql.StandardException;
 import com.foundationdb.sql.parser.*;
 import com.foundationdb.sql.unparser.NodeToString;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.*;
 import org.apache.ibatis.builder.SqlSourceBuilder;
 import org.apache.ibatis.mapping.*;
 import org.apache.ibatis.reflection.MetaObject;
@@ -34,7 +38,10 @@ import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 import org.apache.ibatis.reflection.factory.ObjectFactory;
 import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
 import org.apache.ibatis.reflection.wrapper.ObjectWrapperFactory;
-import org.apache.ibatis.scripting.xmltags.*;
+import org.apache.ibatis.scripting.xmltags.DynamicContext;
+import org.apache.ibatis.scripting.xmltags.DynamicSqlSource;
+import org.apache.ibatis.scripting.xmltags.MixedSqlNode;
+import org.apache.ibatis.scripting.xmltags.SqlNode;
 import org.apache.ibatis.session.Configuration;
 
 import java.util.ArrayList;
@@ -159,7 +166,7 @@ public class SqlUtil {
     }
 
     private static abstract class SimpleParser implements Parser {
-        protected NoOrderByParser noOrderByParser;
+        protected RemoveOrderBy removeOrderBy;
 
         public static Parser newParser(Dialect dialect) {
             Parser parser = null;
@@ -185,16 +192,19 @@ public class SqlUtil {
 
         public void initNoOrderBy(Dialect dialect) {
             try {
+                Class.forName("net.sf.jsqlparser.statement.select.Select");
+                removeOrderBy = new NoOrderByParser2();
+                return;
+            } catch (Exception e) {}
+            try {
                 Class.forName("com.foundationdb.sql.unparser.NodeToString");
-                noOrderByParser = new NoOrderByParser(dialect);
-            } catch (Exception e) {
-
-            }
+                removeOrderBy = new NoOrderByParser(dialect);
+            } catch (Exception e) {}
         }
 
         public String removeOrderBy(String sql) {
-            if (noOrderByParser != null) {
-                return noOrderByParser.removeOrderBy(sql);
+            if (removeOrderBy != null) {
+                return removeOrderBy.removeOrderBy(sql);
             }
             return sql;
         }
@@ -332,10 +342,121 @@ public class SqlUtil {
         }
     }
 
+    private interface RemoveOrderBy{
+        String removeOrderBy(String sql);
+    }
+
+    /**
+     * 一个更好的sql解析工具
+     */
+    public static class NoOrderByParser2 implements RemoveOrderBy {
+        private Map<String, String> CACHE = new HashMap<String, String>();
+
+        public String removeOrderBy(String sql) {
+            if (CACHE.get(sql) != null) {
+                return CACHE.get(sql);
+            }
+            Statement stmt = null;
+            try {
+                stmt = CCJSqlParserUtil.parse(sql);
+            } catch (JSQLParserException e) {
+                //无法解析的直接返回原sql
+                return sql;
+            }
+            Select select = (Select) stmt;
+            SelectBody selectBody = select.getSelectBody();
+            processSelectBody(selectBody);
+            String result = select.toString();
+            CACHE.put(sql, result);
+            return result;
+        }
+
+        public void processSelectBody(SelectBody selectBody) {
+            if (selectBody instanceof PlainSelect) {
+                processPlainSelect((PlainSelect) selectBody);
+            } else if (selectBody instanceof WithItem) {
+                WithItem withItem = (WithItem) selectBody;
+                if (withItem.getSelectBody() != null) {
+                    processSelectBody(withItem.getSelectBody());
+                }
+            } else {
+                SetOperationList operationList = (SetOperationList) selectBody;
+                if (operationList.getPlainSelects() != null && operationList.getPlainSelects().size() > 0) {
+                    List<PlainSelect> plainSelects = operationList.getPlainSelects();
+                    for (PlainSelect plainSelect : plainSelects) {
+                        processPlainSelect(plainSelect);
+                    }
+                }
+                if (!orderByHashParameters(operationList.getOrderByElements())) {
+                    operationList.setOrderByElements(null);
+                }
+            }
+        }
+
+        public void processPlainSelect(PlainSelect plainSelect) {
+            if (!orderByHashParameters(plainSelect.getOrderByElements())) {
+                plainSelect.setOrderByElements(null);
+            }
+            if (plainSelect.getFromItem() != null) {
+                processFromItem(plainSelect.getFromItem());
+            }
+            if (plainSelect.getJoins() != null && plainSelect.getJoins().size() > 0) {
+                List<Join> joins = plainSelect.getJoins();
+                for (Join join : joins) {
+                    if (join.getRightItem() != null) {
+                        processFromItem(join.getRightItem());
+                    }
+                }
+            }
+        }
+
+        public void processFromItem(FromItem fromItem) {
+            if (fromItem instanceof SubJoin) {
+                SubJoin subJoin = (SubJoin) fromItem;
+                if (subJoin.getJoin() != null) {
+                    if (subJoin.getJoin().getRightItem() != null) {
+                        processFromItem(subJoin.getJoin().getRightItem());
+                    }
+                }
+                if (subJoin.getLeft() != null) {
+                    processFromItem(subJoin.getLeft());
+                }
+            } else if (fromItem instanceof SubSelect) {
+                SubSelect subSelect = (SubSelect) fromItem;
+                if (subSelect.getSelectBody() != null) {
+                    processSelectBody(subSelect.getSelectBody());
+                }
+            } else if (fromItem instanceof ValuesList) {
+
+            } else if (fromItem instanceof LateralSubSelect) {
+                LateralSubSelect lateralSubSelect = (LateralSubSelect) fromItem;
+                if (lateralSubSelect.getSubSelect() != null) {
+                    SubSelect subSelect = (SubSelect) (lateralSubSelect.getSubSelect());
+                    if (subSelect.getSelectBody() != null) {
+                        processSelectBody(subSelect.getSelectBody());
+                    }
+                }
+            }
+            //Table时不用处理
+        }
+
+        public boolean orderByHashParameters(List<OrderByElement> orderByElements) {
+            if (orderByElements == null) {
+                return false;
+            }
+            for (OrderByElement orderByElement : orderByElements) {
+                if (orderByElement.toString().toUpperCase().contains("?")) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
     /**
      * 解析 - 去掉order by 语句
      */
-    private static class NoOrderByParser extends NodeToString {
+    private static class NoOrderByParser extends NodeToString implements RemoveOrderBy {
         private SQLParser PARSER = new SQLParser();
         private Dialect dialect;
         private Map<String, String> CACHE = new HashMap<String, String>();
@@ -350,6 +471,7 @@ public class SqlUtil {
                     return CACHE.get(sql);
                 }
                 StatementNode stmt = PARSER.parseStatement(sql);
+                stmt.treePrint();
                 String result = toString(stmt);
                 if (result.indexOf('$') > -1) {
                     result = result.replaceAll("\\$\\d+", "?");
@@ -605,5 +727,11 @@ public class SqlUtil {
         MetaObject sqlObject = forObject(newSqlSource);
         sqlObject.setValue("boundSql.sql", SQLPARSER.getCountSql(sql));
         return newSqlSource;
+    }
+
+    public static void main(String[] args) {
+        SqlUtil sqlUtil = new SqlUtil("mysql");
+        String sql = sqlUtil.SQLPARSER.getCountSql("Select * from `order` o where abc = ?");
+        System.out.println(sql);
     }
 }
