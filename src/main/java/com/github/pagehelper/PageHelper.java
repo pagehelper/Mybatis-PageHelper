c9bdd80163f692afd3861cbdb6584665f0b73cd9
isea533 <abel533@gmail.com>
2015-01-24 09:01:00 +0800

    重构。

--- src/main/java/com/github/pagehelper/PageHelper.java
+++ src/main/java/com/github/pagehelper/PageHelper.java
@@ -26,15 +26,10 @@ package com.github.pagehelper;
 
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.mapping.SqlSource;
 import org.apache.ibatis.plugin.*;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -47,39 +42,8 @@ import java.util.Properties;
 @SuppressWarnings({"rawtypes", "unchecked"})
 @Intercepts(@Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}))
 public class PageHelper implements Interceptor {
-    private static final ThreadLocal<Page> LOCAL_PAGE = new ThreadLocal<Page>();
     //sql工具类
-    private SqlUtil SQLUTIL;
-    //RowBounds参数offset作为PageNum使用 - 默认不使用
-    private boolean offsetAsPageNum = false;
-    //RowBounds是否进行count查询 - 默认不查询
-    private boolean rowBoundsWithCount = false;
-    //当设置为true的时候，如果pagesize设置为0（或RowBounds的limit=0），就不执行分页，返回全部结果
-    private boolean pageSizeZero = false;
-    //分页合理化
-    private boolean reasonable = false;
-    //params参数映射
-    private static Map<String, String> PARAMS = new HashMap<String, String>(5);
-    //request获取方法
-    private static Boolean hasRequest;
-    private static Class<?> requestClass;
-    private static Method getParameter;
-
-    /**
-     * 获取Page参数
-     *
-     * @return
-     */
-    public static Page getLocalPage(){
-        return LOCAL_PAGE.get();
-    }
-
-    /**
-     * 移除本地变量
-     */
-    private static void clearLocalPage(){
-        LOCAL_PAGE.remove();
-    }
+    private SqlUtil sqlUtil;
 
     /**
      * 开始分页
@@ -87,8 +51,8 @@ public class PageHelper implements Interceptor {
      * @param pageNum  页码
      * @param pageSize 每页显示数量
      */
-    public static void startPage(int pageNum, int pageSize) {
-        startPage(pageNum, pageSize, true);
+    public static Page startPage(int pageNum, int pageSize) {
+        return startPage(pageNum, pageSize, true);
     }
 
     /**
@@ -98,8 +62,8 @@ public class PageHelper implements Interceptor {
      * @param pageSize 每页显示数量
      * @param count    是否进行count查询
      */
-    public static void startPage(int pageNum, int pageSize, boolean count) {
-        startPage(pageNum, pageSize, count, null);
+    public static Page startPage(int pageNum, int pageSize, boolean count) {
+        return startPage(pageNum, pageSize, count, null);
     }
 
     /**
@@ -110,8 +74,8 @@ public class PageHelper implements Interceptor {
      * @param count      是否进行count查询
      * @param reasonable 分页合理化,null时用默认配置
      */
-    public static void startPage(int pageNum, int pageSize, boolean count, Boolean reasonable) {
-        startPage(pageNum, pageSize, count, reasonable, null);
+    public static Page startPage(int pageNum, int pageSize, boolean count, Boolean reasonable) {
+        return startPage(pageNum, pageSize, count, reasonable, null);
     }
 
     /**
@@ -123,11 +87,12 @@ public class PageHelper implements Interceptor {
      * @param reasonable   分页合理化,null时用默认配置
      * @param pageSizeZero true且pageSize=0时返回全部结果，false时分页,null时用默认配置
      */
-    public static void startPage(int pageNum, int pageSize, boolean count, Boolean reasonable, Boolean pageSizeZero) {
+    public static Page startPage(int pageNum, int pageSize, boolean count, Boolean reasonable, Boolean pageSizeZero) {
         Page page = new Page(pageNum, pageSize, count);
         page.setReasonable(reasonable);
         page.setPageSizeZero(pageSizeZero);
-        LOCAL_PAGE.set(page);
+        SqlUtil.setLocalPage(page);
+        return page;
     }
 
     /**
@@ -135,109 +100,12 @@ public class PageHelper implements Interceptor {
      *
      * @param params 只能是Map或ServletRequest类型
      */
-    public static void startPage(Object params) {
-        int pageNum = 0;
-        int pageSize = 0;
-        try {
-            pageNum = Integer.parseInt(String.valueOf(getParamValue(params, "pageNum", true)));
-            pageSize = Integer.parseInt(String.valueOf(getParamValue(params, "pageSize", true)));
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("分页参数不是合法的数字类型!");
-        }
-        Object _count = getParamValue(params, "count", false);
-        boolean count = true;
-        if (_count != null) {
-            count = Boolean.valueOf(String.valueOf(_count));
-        }
-        Page page = new Page(pageNum, pageSize, count);
-        Object reasonable = getParamValue(params, "reasonable", false);
-        if (reasonable != null) {
-            page.setReasonable(Boolean.valueOf(String.valueOf(reasonable)));
-        }
-        Object pageSizeZero = getParamValue(params, "pageSizeZero", false);
-        if (pageSizeZero != null) {
-            page.setPageSizeZero(Boolean.valueOf(String.valueOf(pageSizeZero)));
-        }
-        LOCAL_PAGE.set(page);
-    }
-
-    /**
-     * 从对象中取参数
-     *
-     * @param params
-     * @param paramName
-     * @param required
-     * @return
-     */
-    private static Object getParamValue(Object params, String paramName, boolean required) {
-        if (params == null) {
-            throw new NullPointerException("分页查询参数params不能为空!");
-        }
-        Object value = null;
-        if (params instanceof Map) {
-            if (((Map) params).containsKey(PARAMS.get(paramName))) {
-                value = ((Map) params).get(PARAMS.get(paramName));
-            }
-        } else {
-            if (hasRequest == null) {
-                try {
-                    requestClass = Class.forName("javax.servlet.ServletRequest");
-                    getParameter = requestClass.getMethod("getParameter", String.class);
-                    hasRequest = true;
-                } catch (Exception e) {
-                    hasRequest = false;
-                }
-            }
-            if (hasRequest) {
-                try {
-                    if (requestClass.isAssignableFrom(params.getClass())) {
-                        value = getParameter.invoke(params, PARAMS.get(paramName));
-                    } else {
-                        throw new IllegalArgumentException("分页查询参数params类型错误，只能是Map或ServletRequest类型!");
-                    }
-                } catch (IllegalArgumentException e) {
-                    throw e;
-                } catch (Exception e) {
-                    //忽略
-                }
-            } else {
-                throw new IllegalArgumentException("分页查询参数params类型错误，只能是Map或ServletRequest类型!");
-            }
-        }
-        if (required && value == null) {
-            throw new RuntimeException("分页查询缺少必要的参数:" + PARAMS.get(paramName));
-        }
-        return value;
-    }
-
-    /**
-     * 获取分页参数
-     *
-     * @param rowBounds RowBounds参数
-     * @return 返回Page对象
-     */
-    private Page getPage(RowBounds rowBounds) {
-        Page page = LOCAL_PAGE.get();
-        //移除本地变量
-        LOCAL_PAGE.remove();
-        if (page == null) {
-            if (offsetAsPageNum) {
-                page = new Page(rowBounds.getOffset(), rowBounds.getLimit(), rowBoundsWithCount);
-            } else {
-                page = new Page(rowBounds, rowBoundsWithCount);
-            }
-            LOCAL_PAGE.set(page);
-        }
-        //分页合理化
-        if (page.getReasonable() == null) {
-            page.setReasonable(reasonable);
-        }
-        //当设置为true的时候，如果pagesize设置为0（或RowBounds的limit=0），就不执行分页，返回全部结果
-        if (page.getPageSizeZero() == null) {
-            page.setPageSizeZero(pageSizeZero);
-        }
+    public static Page startPage(Object params) {
+        Page page = SqlUtil.getPageFromObject(params);
+        SqlUtil.setLocalPage(page);
         return page;
     }
+
 
     /**
      * Mybatis拦截器方法
@@ -247,65 +115,7 @@ public class PageHelper implements Interceptor {
      * @throws Throwable 抛出异常
      */
     public Object intercept(Invocation invocation) throws Throwable {
-        final Object[] args = invocation.getArgs();
-        RowBounds rowBounds = (RowBounds) args[2];
-        if (LOCAL_PAGE.get() == null && rowBounds == RowBounds.DEFAULT) {
-            return invocation.proceed();
-        } else {
-            //获取原始的ms
-            MappedStatement ms = (MappedStatement) args[0];
-            //忽略RowBounds-否则会进行Mybatis自带的内存分页
-            args[2] = RowBounds.DEFAULT;
-            //分页信息
-            Page page = getPage(rowBounds);
-            //pageSizeZero的判断
-            if ((page.getPageSizeZero() != null && page.getPageSizeZero()) && page.getPageSize() == 0) {
-                //执行正常（不分页）查询
-                Object result = invocation.proceed();
-                //得到处理结果
-                page.addAll((List) result);
-                //相当于查询第一页
-                page.setPageNum(1);
-                //这种情况相当于pageSize=total
-                page.setPageSize(page.size());
-                //仍然要设置total
-                page.setTotal(page.size());
-                //清空page变量
-                clearLocalPage();
-                //返回结果仍然为Page类型 - 便于后面对接收类型的统一处理
-                return page;
-            }
-            SqlSource sqlSource = ((MappedStatement) args[0]).getSqlSource();
-            //简单的通过total的值来判断是否进行count查询
-            if (page.isCount()) {
-                //将参数中的MappedStatement替换为新的qs
-                SQLUTIL.processCountMappedStatement(ms, sqlSource, args);
-                //查询总数
-                Object result = invocation.proceed();
-                //设置总数
-                page.setTotal((Integer) ((List) result).get(0));
-                if (page.getTotal() == 0) {
-                    //清空page变量
-                    clearLocalPage();
-                    return page;
-                }
-            }
-            //pageSize>0的时候执行分页查询，pageSize<=0的时候不执行相当于可能只返回了一个count
-            if (page.getPageSize() > 0 &&
-                    ((rowBounds == RowBounds.DEFAULT && page.getPageNum() > 0)
-                            || rowBounds != RowBounds.DEFAULT)) {
-                //将参数中的MappedStatement替换为新的qs
-                SQLUTIL.processPageMappedStatement(ms, sqlSource, page, args);
-                //执行分页查询
-                Object result = invocation.proceed();
-                //得到处理结果
-                page.addAll((List) result);
-            }
-            //清空page变量
-            clearLocalPage();
-            //返回结果
-            return page;
-        }
+        return sqlUtil.processPage(invocation);
     }
 
     /**
@@ -330,34 +140,7 @@ public class PageHelper implements Interceptor {
     public void setProperties(Properties p) {
         //数据库方言
         String dialect = p.getProperty("dialect");
-        SQLUTIL = new SqlUtil(dialect);
-        //offset作为PageNum使用
-        String offsetAsPageNum = p.getProperty("offsetAsPageNum");
-        this.offsetAsPageNum = Boolean.parseBoolean(offsetAsPageNum);
-        //RowBounds方式是否做count查询
-        String rowBoundsWithCount = p.getProperty("rowBoundsWithCount");
-        this.rowBoundsWithCount = Boolean.parseBoolean(rowBoundsWithCount);
-        //当设置为true的时候，如果pagesize设置为0（或RowBounds的limit=0），就不执行分页
-        String pageSizeZero = p.getProperty("pageSizeZero");
-        this.pageSizeZero = Boolean.parseBoolean(pageSizeZero);
-        //分页合理化，true开启，如果分页参数不合理会自动修正。默认false不启用
-        String reasonable = p.getProperty("reasonable");
-        this.reasonable = Boolean.parseBoolean(reasonable);
-        //参数映射
-        PARAMS.put("pageNum", "pageNum");
-        PARAMS.put("pageSize", "pageSize");
-        PARAMS.put("count", "countSql");
-        PARAMS.put("reasonable", "reasonable");
-        PARAMS.put("pageSizeZero", "pageSizeZero");
-        String params = p.getProperty("params");
-        if (params != null && params.length() > 0) {
-            String[] ps = params.split("[;|,|&]");
-            for (String s : ps) {
-                String[] ss = s.split("[=|:]");
-                if (ss.length == 2) {
-                    PARAMS.put(ss[0], ss[1]);
-                }
-            }
-        }
+        sqlUtil = new SqlUtil(dialect);
+        sqlUtil.setProperties(p);
     }
 }
--- src/main/java/com/github/pagehelper/SqlUtil.java
+++ src/main/java/com/github/pagehelper/SqlUtil.java
@@ -28,6 +28,7 @@ import org.apache.ibatis.builder.SqlSourceBuilder;
 import org.apache.ibatis.builder.StaticSqlSource;
 import org.apache.ibatis.builder.annotation.ProviderSqlSource;
 import org.apache.ibatis.mapping.*;
+import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 import org.apache.ibatis.reflection.factory.ObjectFactory;
@@ -38,11 +39,10 @@ import org.apache.ibatis.scripting.xmltags.DynamicSqlSource;
 import org.apache.ibatis.scripting.xmltags.MixedSqlNode;
 import org.apache.ibatis.scripting.xmltags.SqlNode;
 import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.RowBounds;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Method;
+import java.util.*;
 
 /**
  * Mybatis - sql工具，获取分页和count的MappedStatement，设置分页参数
@@ -53,6 +53,32 @@ import java.util.Map;
  */
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class SqlUtil {
+    private static final ThreadLocal<Page> LOCAL_PAGE = new ThreadLocal<Page>();
+    //RowBounds参数offset作为PageNum使用 - 默认不使用
+    private boolean offsetAsPageNum = false;
+    //RowBounds是否进行count查询 - 默认不查询
+    private boolean rowBoundsWithCount = false;
+    //当设置为true的时候，如果pagesize设置为0（或RowBounds的limit=0），就不执行分页，返回全部结果
+    private boolean pageSizeZero = false;
+    //分页合理化
+    private boolean reasonable = false;
+    //params参数映射
+    private static Map<String, String> PARAMS = new HashMap<String, String>(5);
+    //request获取方法
+    private static Boolean hasRequest;
+    private static Class<?> requestClass;
+    private static Method getParameter;
+
+    static {
+        try {
+            requestClass = Class.forName("javax.servlet.ServletRequest");
+            getParameter = requestClass.getMethod("getParameter", String.class);
+            hasRequest = true;
+        } catch (Exception e) {
+            hasRequest = false;
+        }
+    }
+
     private static final List<ResultMapping> EMPTY_RESULTMAPPING = new ArrayList<ResultMapping>(0);
     //分页的id后缀
     private static final String SUFFIX_PAGE = "_PageHelper";
@@ -90,6 +116,229 @@ public class SqlUtil {
     //数据库方言 - 使用枚举限制数据库类型
     public enum Dialect {
         mysql, mariadb, sqlite, oracle, hsqldb, postgresql
+    }
+
+    public static void setLocalPage(Page page) {
+        LOCAL_PAGE.set(page);
+    }
+
+    /**
+     * 获取Page参数
+     *
+     * @return
+     */
+    public static Page getLocalPage() {
+        return LOCAL_PAGE.get();
+    }
+
+    /**
+     * 移除本地变量
+     */
+    public static void clearLocalPage() {
+        LOCAL_PAGE.remove();
+    }
+
+    /**
+     * 获取分页参数
+     *
+     * @param params RowBounds参数
+     * @return 返回Page对象
+     */
+    public Page getPage(Object params) {
+        Page page = getLocalPage();
+        if (page == null) {
+            if (params instanceof RowBounds) {
+                RowBounds rowBounds = (RowBounds) params;
+                if (offsetAsPageNum) {
+                    page = new Page(rowBounds.getOffset(), rowBounds.getLimit(), rowBoundsWithCount);
+                } else {
+                    page = new Page(rowBounds, rowBoundsWithCount);
+                }
+            } else {
+                page = getPageFromObject(params);
+            }
+            setLocalPage(page);
+        }
+        //分页合理化
+        if (page.getReasonable() == null) {
+            page.setReasonable(reasonable);
+        }
+        //当设置为true的时候，如果pagesize设置为0（或RowBounds的limit=0），就不执行分页，返回全部结果
+        if (page.getPageSizeZero() == null) {
+            page.setPageSizeZero(pageSizeZero);
+        }
+        return page;
+    }
+
+    /**
+     * 从request或Map中获取分页参数
+     *
+     * @param params
+     * @return
+     */
+    public static Page getPageFromObject(Object params) {
+        int pageNum;
+        int pageSize;
+        try {
+            pageNum = Integer.parseInt(String.valueOf(getParamValue(params, "pageNum", true)));
+            pageSize = Integer.parseInt(String.valueOf(getParamValue(params, "pageSize", true)));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("分页参数不是合法的数字类型!");
+        }
+        Object _count = getParamValue(params, "count", false);
+        boolean count = true;
+        if (_count != null) {
+            count = Boolean.valueOf(String.valueOf(_count));
+        }
+        Page page = new Page(pageNum, pageSize, count);
+        Object reasonable = getParamValue(params, "reasonable", false);
+        if (reasonable != null) {
+            page.setReasonable(Boolean.valueOf(String.valueOf(reasonable)));
+        }
+        Object pageSizeZero = getParamValue(params, "pageSizeZero", false);
+        if (pageSizeZero != null) {
+            page.setPageSizeZero(Boolean.valueOf(String.valueOf(pageSizeZero)));
+        }
+        return page;
+    }
+
+    /**
+     * 从对象中取参数
+     *
+     * @param params
+     * @param paramName
+     * @param required
+     * @return
+     */
+    public static Object getParamValue(Object params, String paramName, boolean required) {
+        if (params == null) {
+            throw new NullPointerException("分页查询参数params不能为空!");
+        }
+        Object value = null;
+        if (params instanceof Map) {
+            if (((Map) params).containsKey(PARAMS.get(paramName))) {
+                value = ((Map) params).get(PARAMS.get(paramName));
+            }
+        } else if (hasRequest && requestClass.isAssignableFrom(params.getClass())) {
+            try {
+                value = getParameter.invoke(params, PARAMS.get(paramName));
+            } catch (Exception e) {
+                //忽略
+            }
+        } else {
+            //TODO 其他类型
+        }
+        if (required && value == null) {
+            throw new RuntimeException("分页查询缺少必要的参数:" + PARAMS.get(paramName));
+        }
+        return value;
+    }
+
+    /**
+     * Mybatis拦截器方法
+     *
+     * @param invocation 拦截器入参
+     * @return 返回执行结果
+     * @throws Throwable 抛出异常
+     */
+    public Object processPage(Invocation invocation) throws Throwable {
+        Object result = _processPage(invocation);
+        clearLocalPage();
+        return result;
+    }
+
+    /**
+     * Mybatis拦截器方法
+     *
+     * @param invocation 拦截器入参
+     * @return 返回执行结果
+     * @throws Throwable 抛出异常
+     */
+    private Object _processPage(Invocation invocation) throws Throwable {
+        final Object[] args = invocation.getArgs();
+        RowBounds rowBounds = (RowBounds) args[2];
+        if (SqlUtil.getLocalPage() == null && rowBounds == RowBounds.DEFAULT) {
+            return invocation.proceed();
+        } else {
+            //获取原始的ms
+            MappedStatement ms = (MappedStatement) args[0];
+            //忽略RowBounds-否则会进行Mybatis自带的内存分页
+            args[2] = RowBounds.DEFAULT;
+            //分页信息
+            Page page = getPage(rowBounds);
+            //pageSizeZero的判断
+            if ((page.getPageSizeZero() != null && page.getPageSizeZero()) && page.getPageSize() == 0) {
+                //执行正常（不分页）查询
+                Object result = invocation.proceed();
+                //得到处理结果
+                page.addAll((List) result);
+                //相当于查询第一页
+                page.setPageNum(1);
+                //这种情况相当于pageSize=total
+                page.setPageSize(page.size());
+                //仍然要设置total
+                page.setTotal(page.size());
+                //返回结果仍然为Page类型 - 便于后面对接收类型的统一处理
+                return page;
+            }
+            SqlSource sqlSource = ((MappedStatement) args[0]).getSqlSource();
+            //简单的通过total的值来判断是否进行count查询
+            if (page.isCount()) {
+                //将参数中的MappedStatement替换为新的qs
+                processCountMappedStatement(ms, sqlSource, args);
+                //查询总数
+                Object result = invocation.proceed();
+                //设置总数
+                page.setTotal((Integer) ((List) result).get(0));
+                if (page.getTotal() == 0) {
+                    return page;
+                }
+            }
+            //pageSize>0的时候执行分页查询，pageSize<=0的时候不执行相当于可能只返回了一个count
+            if (page.getPageSize() > 0 &&
+                    ((rowBounds == RowBounds.DEFAULT && page.getPageNum() > 0)
+                            || rowBounds != RowBounds.DEFAULT)) {
+                //将参数中的MappedStatement替换为新的qs
+                processPageMappedStatement(ms, sqlSource, page, args);
+                //执行分页查询
+                Object result = invocation.proceed();
+                //得到处理结果
+                page.addAll((List) result);
+            }
+            //返回结果
+            return page;
+        }
+    }
+
+    public void setProperties(Properties p) {
+        //offset作为PageNum使用
+        String offsetAsPageNum = p.getProperty("offsetAsPageNum");
+        this.offsetAsPageNum = Boolean.parseBoolean(offsetAsPageNum);
+        //RowBounds方式是否做count查询
+        String rowBoundsWithCount = p.getProperty("rowBoundsWithCount");
+        this.rowBoundsWithCount = Boolean.parseBoolean(rowBoundsWithCount);
+        //当设置为true的时候，如果pagesize设置为0（或RowBounds的limit=0），就不执行分页
+        String pageSizeZero = p.getProperty("pageSizeZero");
+        this.pageSizeZero = Boolean.parseBoolean(pageSizeZero);
+        //分页合理化，true开启，如果分页参数不合理会自动修正。默认false不启用
+        String reasonable = p.getProperty("reasonable");
+        this.reasonable = Boolean.parseBoolean(reasonable);
+        //参数映射
+        PARAMS.put("pageNum", "pageNum");
+        PARAMS.put("pageSize", "pageSize");
+        PARAMS.put("count", "countSql");
+        PARAMS.put("reasonable", "reasonable");
+        PARAMS.put("pageSizeZero", "pageSizeZero");
+        String params = p.getProperty("params");
+        if (params != null && params.length() > 0) {
+            String[] ps = params.split("[;|,|&]");
+            for (String s : ps) {
+                String[] ss = s.split("[=|:]");
+                if (ss.length == 2) {
+                    PARAMS.put(ss[0], ss[1]);
+                }
+            }
+        }
     }
 
     /**
