a898c902b8534be3d70a755d0f8ff35cb0c6fdf4
isea533 <abel533@gmail.com>
2015-07-09 17:05:07 +0800

    PageInfo增加空构造方法，所有属性增加setter方法

--- src/main/java/com/github/pagehelper/PageInfo.java
+++ src/main/java/com/github/pagehelper/PageInfo.java
@@ -82,6 +82,9 @@ public class PageInfo<T> implements Serializable {
     //所有导航页号
     private int[] navigatepageNums;
 
+    public PageInfo() {
+    }
+
     /**
      * 包装Page对象
      *
@@ -188,80 +191,148 @@ public class PageInfo<T> implements Serializable {
         hasNextPage = pageNum < pages;
     }
 
-    public void setPageNum(int pageNum) {
-        this.pageNum = pageNum;
-    }
-
     public int getPageNum() {
         return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
     }
 
     public int getPageSize() {
         return pageSize;
     }
 
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
     public int getSize() {
         return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 
     public int getStartRow() {
         return startRow;
     }
 
+    public void setStartRow(int startRow) {
+        this.startRow = startRow;
+    }
+
     public int getEndRow() {
         return endRow;
+    }
+
+    public void setEndRow(int endRow) {
+        this.endRow = endRow;
     }
 
     public long getTotal() {
         return total;
     }
 
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
     public int getPages() {
         return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
     }
 
     public List<T> getList() {
         return list;
     }
 
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+
     public int getFirstPage() {
         return firstPage;
+    }
+
+    public void setFirstPage(int firstPage) {
+        this.firstPage = firstPage;
     }
 
     public int getPrePage() {
         return prePage;
     }
 
+    public void setPrePage(int prePage) {
+        this.prePage = prePage;
+    }
+
     public int getNextPage() {
         return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
     }
 
     public int getLastPage() {
         return lastPage;
     }
 
+    public void setLastPage(int lastPage) {
+        this.lastPage = lastPage;
+    }
+
     public boolean isIsFirstPage() {
         return isFirstPage;
+    }
+
+    public void setIsFirstPage(boolean isFirstPage) {
+        this.isFirstPage = isFirstPage;
     }
 
     public boolean isIsLastPage() {
         return isLastPage;
     }
 
+    public void setIsLastPage(boolean isLastPage) {
+        this.isLastPage = isLastPage;
+    }
+
     public boolean isHasPreviousPage() {
         return hasPreviousPage;
+    }
+
+    public void setHasPreviousPage(boolean hasPreviousPage) {
+        this.hasPreviousPage = hasPreviousPage;
     }
 
     public boolean isHasNextPage() {
         return hasNextPage;
     }
 
+    public void setHasNextPage(boolean hasNextPage) {
+        this.hasNextPage = hasNextPage;
+    }
+
     public int getNavigatePages() {
         return navigatePages;
     }
 
+    public void setNavigatePages(int navigatePages) {
+        this.navigatePages = navigatePages;
+    }
+
     public int[] getNavigatepageNums() {
         return navigatepageNums;
+    }
+
+    public void setNavigatepageNums(int[] navigatepageNums) {
+        this.navigatepageNums = navigatepageNums;
     }
 
     @Override
