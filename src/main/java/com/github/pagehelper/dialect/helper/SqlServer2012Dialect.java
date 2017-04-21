7aba23db4a8b2dd7426d30a796053ef0963d6781
isea533 <abel533@gmail.com>
2017-04-22 06:53:48 +0800

    解决 sqlserver2012 分页错误的问题 [42](https://github.com/pagehelper/Mybatis-PageHelper/issues/42)

--- src/main/java/com/github/pagehelper/dialect/helper/SqlServer2012Dialect.java
+++ src/main/java/com/github/pagehelper/dialect/helper/SqlServer2012Dialect.java
@@ -36,18 +36,14 @@ public class SqlServer2012Dialect extends SqlServerDialect {
     public String getPageSql(String sql, Page page, CacheKey pageKey) {
         StringBuilder sqlBuilder = new StringBuilder(sql.length() + 14);
         sqlBuilder.append(sql);
-        if (page.getStartRow() > 0) {
-            sqlBuilder.append(" OFFSET ");
-            sqlBuilder.append(page.getStartRow());
-            sqlBuilder.append(" ROWS ");
-            pageKey.update(page.getStartRow());
-        }
-        if (page.getPageSize() > 0) {
-            sqlBuilder.append(" FETCH NEXT ");
-            sqlBuilder.append(page.getPageSize());
-            sqlBuilder.append(" ROWS ONLY");
-            pageKey.update(page.getPageSize());
-        }
+        sqlBuilder.append(" OFFSET ");
+        sqlBuilder.append(page.getStartRow());
+        sqlBuilder.append(" ROWS ");
+        pageKey.update(page.getStartRow());
+        sqlBuilder.append(" FETCH NEXT ");
+        sqlBuilder.append(page.getPageSize());
+        sqlBuilder.append(" ROWS ONLY");
+        pageKey.update(page.getPageSize());
         return sqlBuilder.toString();
     }
 
--- src/main/java/com/github/pagehelper/dialect/rowbounds/SqlServer2012RowBoundsDialect.java
+++ src/main/java/com/github/pagehelper/dialect/rowbounds/SqlServer2012RowBoundsDialect.java
@@ -38,18 +38,14 @@ public class SqlServer2012RowBoundsDialect extends SqlServerRowBoundsDialect {
     public String getPageSql(String sql, RowBounds rowBounds, CacheKey pageKey) {
         StringBuilder sqlBuilder = new StringBuilder(sql.length() + 14);
         sqlBuilder.append(sql);
-        if (rowBounds.getOffset() > 0) {
-            sqlBuilder.append(" OFFSET ");
-            sqlBuilder.append(rowBounds.getOffset());
-            sqlBuilder.append(" ROWS ");
-            pageKey.update(rowBounds.getOffset());
-        }
-        if (rowBounds.getLimit() > 0) {
-            sqlBuilder.append(" FETCH NEXT ");
-            sqlBuilder.append(rowBounds.getLimit());
-            sqlBuilder.append(" ROWS ONLY");
-            pageKey.update(rowBounds.getLimit());
-        }
+        sqlBuilder.append(" OFFSET ");
+        sqlBuilder.append(rowBounds.getOffset());
+        sqlBuilder.append(" ROWS ");
+        pageKey.update(rowBounds.getOffset());
+        sqlBuilder.append(" FETCH NEXT ");
+        sqlBuilder.append(rowBounds.getLimit());
+        sqlBuilder.append(" ROWS ONLY");
+        pageKey.update(rowBounds.getLimit());
         return sqlBuilder.toString();
     }
 
