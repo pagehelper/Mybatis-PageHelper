4e5557a770bc0750a78b01b2a1a78aa145b9fbf5
isea533 <abel533@gmail.com>
2018-09-04 07:10:36 +0800

    优化代码结构

--- src/main/java/com/github/pagehelper/PageInterceptor.java
+++ src/main/java/com/github/pagehelper/PageInterceptor.java
@@ -26,6 +26,7 @@ package com.github.pagehelper;
 
 import com.github.pagehelper.cache.Cache;
 import com.github.pagehelper.cache.CacheFactory;
+import com.github.pagehelper.util.ExecutorUtil;
 import com.github.pagehelper.util.MSUtils;
 import com.github.pagehelper.util.StringUtil;
 import org.apache.ibatis.cache.CacheKey;
@@ -33,38 +34,36 @@ import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.*;
-import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
-import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 /**
- * Mybatis - 通用分页拦截器<br/>
- * 项目地址 : http://git.oschina.net/free/Mybatis_PageHelper
+ * Mybatis - 通用分页拦截器
+ * <p>
+ * GitHub: https://github.com/pagehelper/Mybatis-PageHelper
+ * <p>
+ * Gitee : https://gitee.com/free/Mybatis_PageHelper
  *
  * @author liuzh/abel533/isea533
  * @version 5.0.0
  */
 @SuppressWarnings({"rawtypes", "unchecked"})
 @Intercepts(
-    {
-        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
-        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
-    }
+        {
+                @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+                @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
+        }
 )
 public class PageInterceptor implements Interceptor {
-    //缓存count查询的ms
-    protected Cache<String, MappedStatement> msCountMap = null;
     private volatile Dialect dialect;
-    private String default_dialect_class = "com.github.pagehelper.PageHelper";
-    private Field additionalParametersField;
     private String countSuffix = "_COUNT";
+    protected Cache<String, MappedStatement> msCountMap = null;
+    private String default_dialect_class = "com.github.pagehelper.PageHelper";
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
@@ -78,7 +77,7 @@ public class PageInterceptor implements Interceptor {
             CacheKey cacheKey;
             BoundSql boundSql;
             //由于逻辑关系，只会进入一次
-            if(args.length == 4){
+            if (args.length == 4) {
                 //4 个参数时
                 boundSql = ms.getBoundSql(parameter);
                 cacheKey = executor.createCacheKey(ms, parameter, rowBounds, boundSql);
@@ -87,67 +86,23 @@ public class PageInterceptor implements Interceptor {
                 cacheKey = (CacheKey) args[4];
                 boundSql = (BoundSql) args[5];
             }
+            checkDialectExists();
 
-            // Spring bean 方式配置时，如果没有配置属性就不会执行下面的 setProperties 方法，就不会初始化
-            // 因此这里会出现 null 的情况 fixed #26
-            if(dialect == null){
-                synchronized (default_dialect_class){
-                    if(dialect == null){
-                        setProperties(new Properties());
-                    }
-                }
-            }
             List resultList;
             //调用方法判断是否需要进行分页，如果不需要，直接返回结果
             if (!dialect.skip(ms, parameter, rowBounds)) {
-                //反射获取动态参数
-                String msId = ms.getId();
-                Configuration configuration = ms.getConfiguration();
-                Map<String, Object> additionalParameters = (Map<String, Object>) additionalParametersField.get(boundSql);
                 //判断是否需要进行 count 查询
                 if (dialect.beforeCount(ms, parameter, rowBounds)) {
-                    String countMsId = msId + countSuffix;
-                    Long count;
-                    //先判断是否存在手写的 count 查询
-                    MappedStatement countMs = getExistedMappedStatement(configuration, countMsId);
-                    if(countMs != null){
-                        count = executeManualCount(executor, countMs, parameter, boundSql, resultHandler);
-                    } else {
-                        countMs = msCountMap.get(countMsId);
-                        //自动创建
-                        if (countMs == null) {
-                            //根据当前的 ms 创建一个返回值为 Long 类型的 ms
-                            countMs = MSUtils.newCountMappedStatement(ms, countMsId);
-                            msCountMap.put(countMsId, countMs);
-                        }
-                        count = executeAutoCount(executor, countMs, parameter, boundSql, rowBounds, resultHandler);
-                    }
-                    //处理查询总数
-                    //返回 true 时继续分页查询，false 时直接返回
+                    //查询总数
+                    Long count = count(executor, ms, parameter, rowBounds, resultHandler, boundSql);
+                    //处理查询总数，返回 true 时继续分页查询，false 时直接返回
                     if (!dialect.afterCount(count, parameter, rowBounds)) {
                         //当查询总数为 0 时，直接返回空的结果
                         return dialect.afterPage(new ArrayList(), parameter, rowBounds);
                     }
                 }
-                //判断是否需要进行分页查询
-                if (dialect.beforePage(ms, parameter, rowBounds)) {
-                    //生成分页的缓存 key
-                    CacheKey pageKey = cacheKey;
-                    //处理参数对象
-                    parameter = dialect.processParameterObject(ms, parameter, boundSql, pageKey);
-                    //调用方言获取分页 sql
-                    String pageSql = dialect.getPageSql(ms, boundSql, parameter, rowBounds, pageKey);
-                    BoundSql pageBoundSql = new BoundSql(configuration, pageSql, boundSql.getParameterMappings(), parameter);
-                    //设置动态参数
-                    for (String key : additionalParameters.keySet()) {
-                        pageBoundSql.setAdditionalParameter(key, additionalParameters.get(key));
-                    }
-                    //执行分页查询
-                    resultList = executor.query(ms, parameter, RowBounds.DEFAULT, resultHandler, pageKey, pageBoundSql);
-                } else {
-                    //不执行分页的情况下，也不执行内存分页
-                    resultList = executor.query(ms, parameter, RowBounds.DEFAULT, resultHandler, cacheKey, boundSql);
-                }
+                resultList = ExecutorUtil.pageQuery(dialect, executor,
+                        ms, parameter, rowBounds, resultHandler, boundSql, cacheKey);
             } else {
                 //rowBounds用参数值，不使用分页插件处理时，仍然支持默认的内存分页
                 resultList = executor.query(ms, parameter, rowBounds, resultHandler, cacheKey, boundSql);
@@ -159,75 +114,40 @@ public class PageInterceptor implements Interceptor {
     }
 
     /**
-     * 执行手动设置的 count 查询，该查询支持的参数必须和被分页的方法相同
-     *
-     * @param executor
-     * @param countMs
-     * @param parameter
-     * @param boundSql
-     * @param resultHandler
-     * @return
-     * @throws IllegalAccessException
-     * @throws SQLException
+     * Spring bean 方式配置时，如果没有配置属性就不会执行下面的 setProperties 方法，就不会初始化
+     * <p>
+     * 因此这里会出现 null 的情况 fixed #26
      */
-    private Long executeManualCount(Executor executor, MappedStatement countMs,
-                                   Object parameter, BoundSql boundSql,
-                                   ResultHandler resultHandler) throws IllegalAccessException, SQLException {
-        CacheKey countKey = executor.createCacheKey(countMs, parameter, RowBounds.DEFAULT, boundSql);
-        BoundSql countBoundSql = countMs.getBoundSql(parameter);
-        Object countResultList = executor.query(countMs, parameter, RowBounds.DEFAULT, resultHandler, countKey, countBoundSql);
-        Long count = ((Number) ((List) countResultList).get(0)).longValue();
-        return count;
+    private void checkDialectExists() {
+        if (dialect == null) {
+            synchronized (default_dialect_class) {
+                if (dialect == null) {
+                    setProperties(new Properties());
+                }
+            }
+        }
     }
 
-    /**
-     * 执行自动生成的 count 查询
-     *
-     * @param executor
-     * @param countMs
-     * @param parameter
-     * @param boundSql
-     * @param rowBounds
-     * @param resultHandler
-     * @return
-     * @throws IllegalAccessException
-     * @throws SQLException
-     */
-    private Long executeAutoCount(Executor executor, MappedStatement countMs,
-                                   Object parameter, BoundSql boundSql,
-                                   RowBounds rowBounds, ResultHandler resultHandler) throws IllegalAccessException, SQLException {
-        Map<String, Object> additionalParameters = (Map<String, Object>) additionalParametersField.get(boundSql);
-        //创建 count 查询的缓存 key
-        CacheKey countKey = executor.createCacheKey(countMs, parameter, RowBounds.DEFAULT, boundSql);
-        //调用方言获取 count sql
-        String countSql = dialect.getCountSql(countMs, boundSql, parameter, rowBounds, countKey);
-        //countKey.update(countSql);
-        BoundSql countBoundSql = new BoundSql(countMs.getConfiguration(), countSql, boundSql.getParameterMappings(), parameter);
-        //当使用动态 SQL 时，可能会产生临时的参数，这些参数需要手动设置到新的 BoundSql 中
-        for (String key : additionalParameters.keySet()) {
-            countBoundSql.setAdditionalParameter(key, additionalParameters.get(key));
+    private Long count(Executor executor, MappedStatement ms, Object parameter,
+                       RowBounds rowBounds, ResultHandler resultHandler,
+                       BoundSql boundSql) throws SQLException {
+        String countMsId = ms.getId() + countSuffix;
+        Long count;
+        //先判断是否存在手写的 count 查询
+        MappedStatement countMs = ExecutorUtil.getExistedMappedStatement(ms.getConfiguration(), countMsId);
+        if (countMs != null) {
+            count = ExecutorUtil.executeManualCount(executor, countMs, parameter, boundSql, resultHandler);
+        } else {
+            countMs = msCountMap.get(countMsId);
+            //自动创建
+            if (countMs == null) {
+                //根据当前的 ms 创建一个返回值为 Long 类型的 ms
+                countMs = MSUtils.newCountMappedStatement(ms, countMsId);
+                msCountMap.put(countMsId, countMs);
+            }
+            count = ExecutorUtil.executeAutoCount(dialect, executor, countMs, parameter, boundSql, rowBounds, resultHandler);
         }
-        //执行 count 查询
-        Object countResultList = executor.query(countMs, parameter, RowBounds.DEFAULT, resultHandler, countKey, countBoundSql);
-        Long count = (Long) ((List) countResultList).get(0);
         return count;
-    }
-
-    /**
-     * 尝试获取已经存在的在 MS，提供对手写count和page的支持
-     *
-     * @param configuration
-     * @param msId
-     * @return
-     */
-    private MappedStatement getExistedMappedStatement(Configuration configuration, String msId){
-        MappedStatement mappedStatement = null;
-        try {
-            mappedStatement = configuration.getMappedStatement(msId, false);
-        } catch (Throwable t){
-            //ignore
-        }
-        return mappedStatement;
     }
 
     @Override
@@ -254,14 +174,6 @@ public class PageInterceptor implements Interceptor {
         String countSuffix = properties.getProperty("countSuffix");
         if (StringUtil.isNotEmpty(countSuffix)) {
             this.countSuffix = countSuffix;
-        }
-
-        try {
-            //反射获取 BoundSql 中的 additionalParameters 属性
-            additionalParametersField = BoundSql.class.getDeclaredField("additionalParameters");
-            additionalParametersField.setAccessible(true);
-        } catch (NoSuchFieldException e) {
-            throw new PageException(e);
         }
     }
 
---
+++ src/main/java/com/github/pagehelper/util/ExecutorUtil.java
@@ -0,0 +1,184 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 abel533@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.pagehelper.util;
+
+import com.github.pagehelper.Dialect;
+import com.github.pagehelper.PageException;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+import java.lang.reflect.Field;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author liuzenghui
+ */
+public abstract class ExecutorUtil {
+
+    private static Field additionalParametersField;
+
+    static {
+        try {
+            additionalParametersField = BoundSql.class.getDeclaredField("additionalParameters");
+            additionalParametersField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            throw new PageException("获取 BoundSql 属性 additionalParameters 失败: " + e, e);
+        }
+    }
+
+    /**
+     * 获取 BoundSql 属性值 additionalParameters
+     *
+     * @param boundSql
+     * @return
+     */
+    public static Map<String, Object> getAdditionalParameter(BoundSql boundSql) {
+        try {
+            return (Map<String, Object>) additionalParametersField.get(boundSql);
+        } catch (IllegalAccessException e) {
+            throw new PageException("获取 BoundSql 属性值 additionalParameters 失败: " + e, e);
+        }
+    }
+
+    /**
+     * 尝试获取已经存在的在 MS，提供对手写count和page的支持
+     *
+     * @param configuration
+     * @param msId
+     * @return
+     */
+    public static MappedStatement getExistedMappedStatement(Configuration configuration, String msId) {
+        MappedStatement mappedStatement = null;
+        try {
+            mappedStatement = configuration.getMappedStatement(msId, false);
+        } catch (Throwable t) {
+            //ignore
+        }
+        return mappedStatement;
+    }
+
+    /**
+     * 执行手动设置的 count 查询，该查询支持的参数必须和被分页的方法相同
+     *
+     * @param executor
+     * @param countMs
+     * @param parameter
+     * @param boundSql
+     * @param resultHandler
+     * @return
+     * @throws SQLException
+     */
+    public static Long executeManualCount(Executor executor, MappedStatement countMs,
+                                          Object parameter, BoundSql boundSql,
+                                          ResultHandler resultHandler) throws SQLException {
+        CacheKey countKey = executor.createCacheKey(countMs, parameter, RowBounds.DEFAULT, boundSql);
+        BoundSql countBoundSql = countMs.getBoundSql(parameter);
+        Object countResultList = executor.query(countMs, parameter, RowBounds.DEFAULT, resultHandler, countKey, countBoundSql);
+        Long count = ((Number) ((List) countResultList).get(0)).longValue();
+        return count;
+    }
+
+    /**
+     * 执行自动生成的 count 查询
+     *
+     * @param dialect
+     * @param executor
+     * @param countMs
+     * @param parameter
+     * @param boundSql
+     * @param rowBounds
+     * @param resultHandler
+     * @return
+     * @throws SQLException
+     */
+    public static Long executeAutoCount(Dialect dialect, Executor executor, MappedStatement countMs,
+                                        Object parameter, BoundSql boundSql,
+                                        RowBounds rowBounds, ResultHandler resultHandler) throws SQLException {
+        Map<String, Object> additionalParameters = getAdditionalParameter(boundSql);
+        //创建 count 查询的缓存 key
+        CacheKey countKey = executor.createCacheKey(countMs, parameter, RowBounds.DEFAULT, boundSql);
+        //调用方言获取 count sql
+        String countSql = dialect.getCountSql(countMs, boundSql, parameter, rowBounds, countKey);
+        //countKey.update(countSql);
+        BoundSql countBoundSql = new BoundSql(countMs.getConfiguration(), countSql, boundSql.getParameterMappings(), parameter);
+        //当使用动态 SQL 时，可能会产生临时的参数，这些参数需要手动设置到新的 BoundSql 中
+        for (String key : additionalParameters.keySet()) {
+            countBoundSql.setAdditionalParameter(key, additionalParameters.get(key));
+        }
+        //执行 count 查询
+        Object countResultList = executor.query(countMs, parameter, RowBounds.DEFAULT, resultHandler, countKey, countBoundSql);
+        Long count = (Long) ((List) countResultList).get(0);
+        return count;
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param dialect
+     * @param executor
+     * @param ms
+     * @param parameter
+     * @param rowBounds
+     * @param resultHandler
+     * @param boundSql
+     * @param cacheKey
+     * @param <E>
+     * @return
+     * @throws SQLException
+     */
+    public static  <E> List<E> pageQuery(Dialect dialect, Executor executor, MappedStatement ms, Object parameter,
+                                 RowBounds rowBounds, ResultHandler resultHandler,
+                                 BoundSql boundSql, CacheKey cacheKey) throws SQLException {
+        //判断是否需要进行分页查询
+        if (dialect.beforePage(ms, parameter, rowBounds)) {
+            //生成分页的缓存 key
+            CacheKey pageKey = cacheKey;
+            //处理参数对象
+            parameter = dialect.processParameterObject(ms, parameter, boundSql, pageKey);
+            //调用方言获取分页 sql
+            String pageSql = dialect.getPageSql(ms, boundSql, parameter, rowBounds, pageKey);
+            BoundSql pageBoundSql = new BoundSql(ms.getConfiguration(), pageSql, boundSql.getParameterMappings(), parameter);
+
+            Map<String, Object> additionalParameters = getAdditionalParameter(boundSql);
+            //设置动态参数
+            for (String key : additionalParameters.keySet()) {
+                pageBoundSql.setAdditionalParameter(key, additionalParameters.get(key));
+            }
+            //执行分页查询
+            return executor.query(ms, parameter, RowBounds.DEFAULT, resultHandler, pageKey, pageBoundSql);
+        } else {
+            //不执行分页的情况下，也不执行内存分页
+            return executor.query(ms, parameter, RowBounds.DEFAULT, resultHandler, cacheKey, boundSql);
+        }
+    }
+
+}
