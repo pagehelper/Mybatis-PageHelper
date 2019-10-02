5a70376033e44890706c2096c0695eb5e5bb5fa4
Enrico Olivelli <enrico.olivelli@diennea.com>
2019-10-02 14:40:51 +0200

    Add support for HerdDB - support HerdDB, mostly like MySQL - auto detect HerdDB

---
+++ src/main/java/com/github/pagehelper/dialect/rowbounds/HerdDBRowBoundsDialect.java
@@ -0,0 +1,56 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 abel533@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.pagehelper.dialect.rowbounds;
+
+import com.github.pagehelper.dialect.AbstractRowBoundsDialect;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.session.RowBounds;
+
+/**
+ * HerdDB 基于 RowBounds 的分页
+ *
+ * @author Enrico Olivelli
+ */
+public class HerdDBRowBoundsDialect extends AbstractRowBoundsDialect {
+
+    @Override
+    public String getPageSql(String sql, RowBounds rowBounds, CacheKey pageKey) {
+        StringBuilder sqlBuilder = new StringBuilder(sql.length() + 14);
+        sqlBuilder.append(sql);
+        if (rowBounds.getOffset() == 0) {
+            sqlBuilder.append(" LIMIT ");
+            sqlBuilder.append(rowBounds.getLimit());
+        } else {
+            sqlBuilder.append(" LIMIT ");
+            sqlBuilder.append(rowBounds.getOffset());
+            sqlBuilder.append(",");
+            sqlBuilder.append(rowBounds.getLimit());
+            pageKey.update(rowBounds.getOffset());
+        }
+        pageKey.update(rowBounds.getLimit());
+        return sqlBuilder.toString();
+    }
+
+}
