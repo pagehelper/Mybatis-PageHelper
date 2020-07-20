6c64fc7bf137eee14974f35d2027ab8c2db8fb96
abel533 <abel533@gmail.com>
2020-07-20 23:38:15 +0800

    理解错误，还原代码 closed #534

--- src/main/java/com/github/pagehelper/Page.java
+++ src/main/java/com/github/pagehelper/Page.java
@@ -124,7 +124,7 @@ public class Page<E> extends ArrayList<E> implements Closeable {
             this.pageSize = 0;
         } else {
             this.pageSize = rowBounds[1];
-            this.pageNum = rowBounds[1] != 0 ? (int) (Math.floor(((double) rowBounds[0] + rowBounds[1]) / rowBounds[1])) : 0;
+            this.pageNum = rowBounds[1] != 0 ? (int) (Math.ceil(((double) rowBounds[0] + rowBounds[1]) / rowBounds[1])) : 0;
         }
         this.startRow = rowBounds[0];
         this.count = count;
