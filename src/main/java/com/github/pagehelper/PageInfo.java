ce3bfb0fed6ca6d6585f7ccdb843704c03b7009e
liuzh <abel533@gmail.com>
2021-06-06 21:38:34 +0800

    规范注释, fixed #547

--- src/main/java/com/github/pagehelper/PageInfo.java
+++ src/main/java/com/github/pagehelper/PageInfo.java
@@ -40,43 +40,74 @@ import java.util.List;
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class PageInfo<T> extends PageSerializable<T> {
     public static final int DEFAULT_NAVIGATE_PAGES = 8;
-    //当前页
+    /**
+     * 当前页
+     */
     private int pageNum;
-    //每页的数量
+    /**
+     * 每页的数量
+     */
     private int pageSize;
-    //当前页的数量
+    /**
+     * 当前页的数量
+     */
     private int size;
 
-    //由于startRow和endRow不常用，这里说个具体的用法
-    //可以在页面中"显示startRow到endRow 共size条数据"
-
-    //当前页面第一个元素在数据库中的行号
+    /**
+     * 由于startRow和endRow不常用，这里说个具体的用法
+     * 可以在页面中"显示startRow到endRow 共size条数据"
+     * 当前页面第一个元素在数据库中的行号
+     */
     private long startRow;
-    //当前页面最后一个元素在数据库中的行号
+    /**
+     * 当前页面最后一个元素在数据库中的行号
+     */
     private long endRow;
-    //总页数
+    /**
+     * 总页数
+     */
     private int pages;
-
-    //前一页
+    /**
+     * 前一页
+     */
     private int prePage;
-    //下一页
+    /**
+     * 下一页
+     */
     private int nextPage;
 
-    //是否为第一页
+    /**
+     * 是否为第一页
+     */
     private boolean isFirstPage = false;
-    //是否为最后一页
+    /**
+     * 是否为最后一页
+     */
     private boolean isLastPage = false;
-    //是否有前一页
+    /**
+     * 是否有前一页
+     */
     private boolean hasPreviousPage = false;
-    //是否有下一页
+    /**
+     * 是否有下一页
+     */
     private boolean hasNextPage = false;
-    //导航页码数
+
+    /**
+     * 导航页码数
+     */
     private int navigatePages;
-    //所有导航页号
+    /**
+     * 所有导航页号
+     */
     private int[] navigatepageNums;
-    //导航条上的第一页
+    /**
+     * 导航条上的第一页
+     */
     private int navigateFirstPage;
-    //导航条上的最后一页
+    /**
+     * 导航条上的最后一页
+     */
     private int navigateLastPage;
 
     public PageInfo() {
