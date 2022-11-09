b2abd2e9c3f4f8521559d6282ff40896eb478291
gonggy <514592053@qq.com>
2022-11-09 09:56:55 +0800

    修复sql注入检验不正确问题

--- src/test/java/com/github/pagehelper/util/SqlSafeUtilTest.java
+++ src/test/java/com/github/pagehelper/util/SqlSafeUtilTest.java
@@ -8,17 +8,28 @@ public class SqlSafeUtilTest {
 
 	@Test
 	public void check() {
-		String sql = "insert into xx";
-		assertTrue(SqlSafeUtil.check(sql));
-
-		sql = "insertxxinto xx";
-		assertFalse(SqlSafeUtil.check(sql));
-
-		sql = "insert           into xx";
-		assertTrue(SqlSafeUtil.check(sql));
+		assertSql(true, "insert into xx");
+		// 无空格
+		assertSql(false, "insertxxinto xx");
+		assertSql(false, "insert_into");
+		assertSql(true, "SELECT aa FROM user");
+		// 无空格
+		assertSql(true, "SELECT*FROM user");
+		// 左空格
+		assertSql(true, "SELECT *FROM user");
+		// 右空格
+		assertSql(true, "SELECT* FROM user");
+		// 左tab
+		assertSql(true, "SELECT                 *FROM user");
+		// 右tab
+		assertSql(true, "SELECT*        FROM user");
+		assertSql(false, "SELECT*FROMuser");
 
 		// 验证 issue #707 问题
-		sql = "databaseType desc,orderNum desc";
-		assertFalse(SqlSafeUtil.check(sql));
+		assertSql(false, "databaseType desc,orderNum desc");
+	}
+
+	private void assertSql(boolean injection, String sql) {
+		assertEquals(injection, SqlSafeUtil.check(sql));
 	}
 }
